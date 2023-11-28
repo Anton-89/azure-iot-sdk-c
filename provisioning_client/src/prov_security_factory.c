598b2e48ba288f7b7f3aa782d2d3acd3b7496440
Anton <134565426+Anton-89@users.noreply.github.com>
2023-11-28 05:08:21 +0200

    Update prov_security_factory.c

--- provisioning_client/src/prov_security_factory.c
+++ provisioning_client/src/prov_security_factory.c
@@ -159,7 +159,7 @@ int prov_dev_set_symmetric_key_info(const char* registration_name, const char* s
             g_symm_key = temp_key;
 
             LogError("&&g_symm_key_reg_name is %s and g_symm_key is %s", g_symm_key_reg_name, g_symm_key);
-            
+            LogError("&&g_symm_key_reg_name is %p, and g_symm_key is %p", (void *)g_symm_key_reg_name, (void*)g_symm_key);
             // Sync dps with iothub only if it is NULL
             if (iothub_security_get_symmetric_key() == NULL || iothub_security_get_symm_registration_name() == NULL)
             {
