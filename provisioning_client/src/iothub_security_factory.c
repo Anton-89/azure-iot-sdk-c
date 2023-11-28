1f4af5ca8db5b74c63164390852f1559bf73b20d
Anton <134565426+Anton-89@users.noreply.github.com>
2023-11-28 16:00:32 +0200

    Update iothub_security_factory.c

--- provisioning_client/src/iothub_security_factory.c
+++ provisioning_client/src/iothub_security_factory.c
@@ -2,80 +2,88 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdlib.h>
-#include "azure_prov_client/iothub_security_factory.h"
-#include "azure_prov_client/prov_security_factory.h"
+#include "azure_c_shared_utility/gballoc.h"
 #include "azure_c_shared_utility/xlogging.h"
 #include "azure_c_shared_utility/crt_abstractions.h"
+#include "azure_prov_client/prov_security_factory.h"
+#include "azure_prov_client/iothub_security_factory.h"
 
 #include "hsm_client_data.h"
 
-static IOTHUB_SECURITY_TYPE g_security_type = IOTHUB_SECURITY_TYPE_UNKNOWN;
+static SECURE_DEVICE_TYPE * g_device_hsm_type = NULL; //SECURE_DEVICE_TYPE_UNKNOWN; //SECURE_DEVICE_TYPE_SYMMETRIC_KEY
 static char* g_symm_key = NULL;
 static char* g_symm_key_reg_name = NULL;
 
-static SECURE_DEVICE_TYPE get_secure_device_type(IOTHUB_SECURITY_TYPE sec_type)
+static IOTHUB_SECURITY_TYPE get_iothub_security_type(SECURE_DEVICE_TYPE sec_type)
 {
-    SECURE_DEVICE_TYPE ret;
+    IOTHUB_SECURITY_TYPE ret;
 
     switch (sec_type)
     {
 #if defined(HSM_TYPE_SAS_TOKEN)  || defined(HSM_AUTH_TYPE_CUSTOM)
-        case IOTHUB_SECURITY_TYPE_SAS:
-            ret = SECURE_DEVICE_TYPE_TPM;
+        case SECURE_DEVICE_TYPE_TPM:
+            ret = IOTHUB_SECURITY_TYPE_SAS;
             break;
 #endif
 
 #if defined(HSM_TYPE_X509) || defined(HSM_TYPE_RIOT) || defined(HSM_AUTH_TYPE_CUSTOM)
-        case IOTHUB_SECURITY_TYPE_X509:
-            ret = SECURE_DEVICE_TYPE_X509;
+        case SECURE_DEVICE_TYPE_X509:
+            ret = IOTHUB_SECURITY_TYPE_X509;
             break;
 #endif
 
 #if defined(HSM_TYPE_SYMM_KEY) || defined(HSM_AUTH_TYPE_CUSTOM)
-        case IOTHUB_SECURITY_TYPE_SYMMETRIC_KEY:
-            ret = SECURE_DEVICE_TYPE_SYMMETRIC_KEY;
+        case SECURE_DEVICE_TYPE_SYMMETRIC_KEY:
+            ret = IOTHUB_SECURITY_TYPE_SYMMETRIC_KEY;
             break;
 #endif
 
 #ifdef HSM_TYPE_HTTP_EDGE
-        case IOTHUB_SECURITY_TYPE_HTTP_EDGE:
-            ret = SECURE_DEVICE_TYPE_HTTP_EDGE;
+        case SECURE_DEVICE_TYPE_HTTP_EDGE:
+            ret = IOTHUB_SECURITY_TYPE_HTTP_EDGE;
             break;
 #endif
 
         default:
-            ret = SECURE_DEVICE_TYPE_UNKNOWN;
+            ret = IOTHUB_SECURITY_TYPE_UNKNOWN;
             break;
     }
 
     return ret;
 }
 
-int iothub_security_init(IOTHUB_SECURITY_TYPE sec_type)
+int prov_dev_security_init(SECURE_DEVICE_TYPE hsm_type)
 {
     int result;
 
-    SECURE_DEVICE_TYPE secure_device_type_from_caller = get_secure_device_type(sec_type);
-    LogError("**iothub_security_init is called\n");
-    LogError("**secure_device_type_from_caller %d and SECURE_DEVICE_TYPE_UNKNOWN is %d\n", secure_device_type_from_caller, SECURE_DEVICE_TYPE_UNKNOWN);
+    IOTHUB_SECURITY_TYPE security_type_from_caller = get_iothub_security_type(hsm_type);
+
+    LogError("--prov_dev_security_init() is called\n");
+    LogError("--security_type_from_caller is %d\n", security_type_from_caller);
     
-    if (secure_device_type_from_caller == SECURE_DEVICE_TYPE_UNKNOWN)
+    if (security_type_from_caller == IOTHUB_SECURITY_TYPE_UNKNOWN)
     {
-        LogError("Security type %d is not supported in this SDK build", sec_type);
+        LogError("HSM type %d is not supported on this SDK build", hsm_type);
         result = MU_FAILURE;
     }
     else
     {
-        g_security_type = sec_type;
-        SECURE_DEVICE_TYPE security_device_type_from_prov = prov_dev_security_get_type();
-        if (security_device_type_from_prov == SECURE_DEVICE_TYPE_UNKNOWN)
-        {
-            LogError("**trying to prov_dev_security_init with %d\n", secure_device_type_from_caller);
-            result = prov_dev_security_init(secure_device_type_from_caller);
+        //g_device_hsm_type = hsm_type;
+        if (g_device_hsm_type == NULL) {
+            g_device_hsm_type = calloc(1, sizeof (SECURE_DEVICE_TYPE));
+            LogError("--g_device_hsm_type is created (%p)", (void *)g_device_hsm_type);
         }
-        else if (secure_device_type_from_caller != security_device_type_from_prov)
+        *g_device_hsm_type = hsm_type;
+        LogError("--g_device_hsm_type is updated as %d\n", *g_device_hsm_type);
+        IOTHUB_SECURITY_TYPE security_type_from_iot = iothub_security_type();
+        if (security_type_from_iot == IOTHUB_SECURITY_TYPE_UNKNOWN)
         {
-            LogError("Security type from caller %d (which maps to security device type %d) does not match already specified security device type %d", sec_type, secure_device_type_from_caller, security_device_type_from_prov);
+            // Initialize iothub_security layer if not currently
+            result = iothub_security_init(security_type_from_caller);
+        }
+        else if (security_type_from_iot != security_type_from_caller)
+        {
+            LogError("Security HSM from caller %d (which maps to security type %d) does not match already specified security type %d", hsm_type, security_type_from_caller, security_type_from_iot);
             result = MU_FAILURE;
         }
         else
@@ -85,13 +93,14 @@ int iothub_security_init(IOTHUB_SECURITY_TYPE sec_type)
 
         if (result == 0)
         {
+            LogError("--trying to initialize_hsm_system\n");
             result = initialize_hsm_system();
         }
     }
     return result;
 }
 
-void iothub_security_deinit()
+void prov_dev_security_deinit(void)
 {
     if (g_symm_key != NULL)
     {
@@ -104,21 +113,24 @@ void iothub_security_deinit()
         g_symm_key_reg_name = NULL;
     }
     deinitialize_hsm_system();
-    if (prov_dev_get_symmetric_key() != NULL || prov_dev_get_symm_registration_name() != NULL)
+    if (iothub_security_get_symmetric_key() != NULL || iothub_security_get_symm_registration_name() != NULL)
     {
-        prov_dev_security_deinit();
+        // Clear out iothub info
+        iothub_security_deinit();
     }
 }
 
-IOTHUB_SECURITY_TYPE iothub_security_type()
+SECURE_DEVICE_TYPE prov_dev_security_get_type(void)
 {
-    return g_security_type;
+    if (g_device_hsm_type == NULL)
+        return SECURE_DEVICE_TYPE_UNKNOWN;
+    
+    return (SECURE_DEVICE_TYPE)*g_device_hsm_type;
 }
 
-int iothub_security_set_symmetric_key_info(const char* registration_name, const char* symmetric_key)
+int prov_dev_set_symmetric_key_info(const char* registration_name, const char* symmetric_key)
 {
     int result;
-    LogError("++iothub_security_set_symmetric_key_info is called");
     if (registration_name == NULL || symmetric_key == NULL)
     {
         LogError("Invalid parameter specified reg_name: %p, symm_key: %p", registration_name, symmetric_key);
@@ -151,11 +163,11 @@ int iothub_security_set_symmetric_key_info(const char* registration_name, const 
             }
             g_symm_key_reg_name = temp_name;
             g_symm_key = temp_key;
-            LogError("++g_symm_key_reg_name is %p, g_symm_key is %p", g_symm_key_reg_name, g_symm_key);
-            // Sync iothub with dps
-            if (prov_dev_get_symmetric_key() == NULL || prov_dev_get_symm_registration_name() == NULL)
+
+            // Sync dps with iothub only if it is NULL
+            if (iothub_security_get_symmetric_key() == NULL || iothub_security_get_symm_registration_name() == NULL)
             {
-                if (prov_dev_set_symmetric_key_info(g_symm_key_reg_name, g_symm_key) != 0)
+                if (iothub_security_set_symmetric_key_info(g_symm_key_reg_name, g_symm_key) != 0)
                 {
                     LogError("Failure syncing dps & IoThub key information");
                     result = MU_FAILURE;
@@ -174,14 +186,12 @@ int iothub_security_set_symmetric_key_info(const char* registration_name, const 
     return result;
 }
 
-const char* iothub_security_get_symmetric_key()
+const char* prov_dev_get_symmetric_key(void)
 {
-    LogError("iothub_security_get_symmetric_key is called %p", (void *)(g_symm_key));
     return g_symm_key;
 }
 
-const char* iothub_security_get_symm_registration_name()
+const char* prov_dev_get_symm_registration_name(void)
 {
-    LogError("iothub_security_get_symm_registration_name is called %p", (void *)(g_symm_key_reg_name));
     return g_symm_key_reg_name;
 }
